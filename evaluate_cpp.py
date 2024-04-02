fc4faadf594231d70cb250d037ae609e3832f299
Luoyan Zhang <zhang.luoya@northeastern.edu>
2024-04-02 15:34:59 -0400

    Update evaluate_cpp

--- evaluate_cpp.py
+++ evaluate_cpp.py
@@ -2,38 +2,42 @@ import os
 import sys
 import subprocess
 
-def compile_file(root, file):
+
+def compile_and_run(root, file):
     # Generate Paths for the files
     src = os.path.join(root, file)
     # Remove the '.c' extension for the output file
-    dst = os.path.join(root, os.path.splitext(file)[0])
-
-    # Compile using gcc
-    subprocess.run(['gcc', src, '-o', dst], check=True)
-
-    # Execution
-    subprocess.run("./" + dst)
+    executable = os.path.splitext(file)[0]
+    dst = os.path.join(root, executable)
+    while True:
+        # Compile using gcc
+        subprocess.run(['gcc', src, '-o', dst], check=True)
+        # Execution
+        args = input(f"?? Enter args:\n  $ ./{executable} ").split(" ")
+        subprocess.run(["./" + dst] + args)
+        if input(f"?? Execute {executable} again? [y/N]") != 'y':
+            break
     os.remove(dst) # Cleanup
 
-def evaluate_file(executable):
-    subprocess.run("./" + executable)
-    os.remove(executable) # Cleanup
 
 def main(root_dir):
     for root, dirs, files in os.walk(root_dir):
         for file in files:
-            if file.endswith('.c'):
-                student = os.path.basename(os.path.dirname(os.path.join(root, file)))
-                # Compile this file
-                input(f"Press Enter to Compile and Evaluate {student} {file}")
-                while True:
-                    try:
-                        dst = compile_file(root, file)
-                    except subprocess.CalledProcessError:
-                        if input(f"Failed! Re-compile {student} {file}? [y/N]") == 'y':
-                            continue
-                        print(f"Skipping Evaluation of {student} {file} due to unsuccessful compilation")
-                    break
+            if not file.endswith('.c'):
+                continue
+            student = os.path.basename(os.path.dirname(os.path.join(root, file)))
+            # Compile this file
+            print(f"\n++ Compiling {student} {file} ...")
+            # if input(f"Do you want to skip this file? [y/N]") == 'y':
+                # continue
+            while True:
+                try:
+                    dst = compile_and_run(root, file)
+                except subprocess.CalledProcessError:
+                    if input(f"?? Failed! Re-compile {student} {file}? [y/N]") == 'y':
+                        continue
+                    print(f"++ Skipping Evaluation of {student} {file} due to unsuccessful compilation")
+                break
 
 
 if __name__ == "__main__":
