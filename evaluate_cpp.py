54e0226649dcaea5d79781c2a87b5962a73c1b0a
Luoyan Zhang <zhang.luoya@northeastern.edu>
2024-04-12 01:57:06 -0400

    Add support for creating/accessing files inside students' directory by using chdir

--- evaluate_cpp.py
+++ evaluate_cpp.py
@@ -4,20 +4,20 @@ import subprocess
 
 
 def compile_and_run(root, file):
-    # Generate Paths for the files
-    src = os.path.join(root, file)
     # Remove the '.c' extension for the output file
     executable = os.path.splitext(file)[0]
-    dst = os.path.join(root, executable)
+
     while True:
         # Compile using gcc
-        subprocess.run(['gcc', src, '-o', dst], check=True)
+        subprocess.run(['gcc', file, '-o', executable], check=True)
         # Execution
         args = input(f"?? Enter args:\n  $ ./{executable} ").split(" ")
-        subprocess.run(["./" + dst] + args)
+        subprocess.run(["./" + executable] + args)
         if input(f"?? Execute {executable} again? [y/N]") != 'y':
             break
-    os.remove(dst) # Cleanup
+    os.remove(executable) # Cleanup
+
+
 
 
 def main(root_dir):
@@ -25,6 +25,7 @@ def main(root_dir):
         for file in files:
             if not file.endswith('.c'):
                 continue
+            cwd = os.getcwd() # Save cwd
             student = os.path.basename(os.path.dirname(os.path.join(root, file)))
             # Compile this file
             print(f"\n++ Compiling {student} {file} ...")
@@ -32,14 +33,20 @@ def main(root_dir):
                 # continue
             while True:
                 try:
+                    os.chdir(os.path.dirname(os.path.join(root, file)))
                     dst = compile_and_run(root, file)
                 except subprocess.CalledProcessError:
                     if input(f"?? Failed! Re-compile {student} {file}? [y/N]") == 'y':
                         continue
                     print(f"++ Skipping Evaluation of {student} {file} due to unsuccessful compilation")
+                finally:
+                    os.chdir(cwd) # Move back to original working directory
                 break
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <Homework directory>")
+        exit(1)
     # Pass in the directory as arguments e.g. 'Homework 2/'
     main(sys.argv[1])
